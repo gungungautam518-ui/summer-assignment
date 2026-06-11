3545aaea5794100b8d0de2f6ab621295c8ae0f1d
Gungun gautam <gungungautam518-ui>
2026-06-11 18:17:13 +0530

    daay 5 assignment

---
+++ day5/quesstion1.py
@@ -0,0 +1,15 @@
+n = int(input("enter the no to check perfect no ."))
+sum = 0
+for i in range(1, n):
+    if (n % i == 0):
+        sum = sum + i   
+
+if (sum == n ):
+    print("the no is perfect ")
+else :
+    print("the no is not perfect")                                                                                     
+
+
+
+
+
