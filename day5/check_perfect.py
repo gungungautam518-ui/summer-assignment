be84db613a16f7ed610b4e15c75b21a1c839eca0
Gungun gautam <gungungautam518@gmail.com>
2026-06-13 18:49:32 +0530

    check perfect num

---
+++ day5/check_perfect.py
@@ -0,0 +1,15 @@
+n = int(input("enter the no to check perfect no ."))
+sum = 0
+for i in range(1, n):
+    if (n % i == 0):
+        sum = sum + i   
+
+if (sum == num ):
+    print("the no is perfect ")
+else :
+    print("the no is not perfect")                                                                                     
+
+ 
+
+
+
